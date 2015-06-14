d46181a95ee70ee9f4dec777495bba084ea47fc2
meijieru <1079315718@qq.com>
2015-06-14 16:19:01 +0800

    adt

---
+++ ADT/queue.h
@@ -0,0 +1,14 @@
+#ifndef __QUEUE_H
+#define __QUEUE_H
+
+
+typedef Queue *Queue_ptr;
+
+void Queue_init(Queue_ptr queue,int capacity);
+void Queue_enqueue(Queue_ptr queue,int target);
+int  Queue_dequeue(Queue_ptr queue);
+void Queue_delete(Queue_ptr queue);
+void Queue_empty(Queue_ptr queue);
+
+
+#endif // __QUEUE_H
